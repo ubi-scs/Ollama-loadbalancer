aac5da7a5a41af943c48895b6d60c9fed375f033
hvoss <hvoss@techfak.uni-bielefeld.de>
2025-08-26 14:35:18 +0200

    crash fix

--- server_application/gui.py
+++ server_application/gui.py
@@ -33,7 +33,7 @@ MODELS_CONFIG_PATH = 'models.csv'
 
 def get_worker_status():
     status_data = []
-    get_worker_models()  # Ensure models file exists
+
     workers = pd.read_csv(WORKER_CONFIG_PATH)
 
     try:
@@ -888,6 +888,9 @@ def main():
     AUTHORIZED_USERS_CONFIG_PATH = str(os.path.join(os.path.dirname(os.path.abspath(__file__)), args.users_list))
     LOG_FILE_PATH = str(os.path.join(os.path.dirname(os.path.abspath(__file__)), args.log_path))
     MODELS_PATH = str(os.path.join(os.path.dirname(os.path.abspath(__file__)), args.models))
+    get_worker_models()
+    get_users()
+    get_global_models()
 
     print("Ollama Proxy server")
     print(f"Configuration file: {WORKER_CONFIG_PATH}")
