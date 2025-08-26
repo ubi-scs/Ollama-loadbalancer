55610e95a6b8020f5b8e25af0794698c1a5b3176
hvoss <hvoss@techfak.uni-bielefeld.de>
2025-08-26 16:57:40 +0200

    remove "Bearer " from authorization header in client initialization

--- server_application/test.py
+++ server_application/test.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     USER = ""
     KEY = ""
 
-    client = ollama.Client(host=PROXY_HOST_URL, headers={"Authorization": f"Bearer {USER}:{KEY}"})
+    client = ollama.Client(host=PROXY_HOST_URL, headers={"Authorization": f"{USER}:{KEY}"})
     response = client.chat(model='qwen:0.5b', messages=[{'role': 'user', 'content': 'How are you?'}])
     print("Response:")
     print(f"Content: {response['message']['content']}")
